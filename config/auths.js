e6b67df0f96ab23fb8fedd49404372d9f2718469
TamandeepSingh <tamandeepsinghkhamba@gmail.com>
2018-03-08 18:17:52 +0530

    updations

---
+++ config/auths.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "facebook" : {
+    "clientId" : "1272226326123675",
+    "clientSecret" : "718f9f7a9c7c6a2dc9efae4fcdb4c39a",
+    "callbackUrl" : "http://localhost:3000/#/auth/facebook/callback"
+  }
+}
---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	"database": "mongodb://localhost/my_database",
+	"port": process.env.PORT || 3000,
+	"secretkey": "taman"
+
+}
